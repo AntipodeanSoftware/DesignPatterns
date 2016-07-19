5747be23e919d5abb9bb3cdb30dbdfa154d6927f
AntipodeanSoftware <darryl.wilson.nz@gmail.com>
2016-07-20 06:27:41 +1200

    Added Singleton Pattern

---
+++ DesignPatternTests/SIngletonTests/BroadcastSingletonTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DesignPatterns;
+using NUnit.Framework;
+
+namespace DesignPatternTests
+{
+	[TestFixture()]
+	public class BroadcastSingletonTests
+	{
+		public BroadcastSingletonTests()
+		{
+		}
+
+		[Test()]
+		public void BroadcastSingletonTest()
+		{
+			BroadcastSingleton broadcast = BroadcastSingleton.CreateBroadcastSingleton();
+
+			Assert.IsNotNull(broadcast);
+
+		}
+	}
+}
+
---
+++ DesignPatterns/Singleton/BroadcastSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace DesignPatterns
+{
+	public class BroadcastSingleton
+	{
+		private volatile static BroadcastSingleton instance;
+
+		private static object syncLock = new object();
+
+		private BroadcastSingleton()
+		{
+		}
+
+		public static BroadcastSingleton CreateBroadcastSingleton()
+		{
+			if (instance == null)
+			{
+				lock (syncLock)
+				{
+					if (instance == null)
+					{
+						instance = new BroadcastSingleton();
+					}
+				}
+			}
+
+			return instance;
+
+		}
+	}
+}
+
